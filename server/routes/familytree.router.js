e2fca3f89d3a351b5a4d9c66b3d2febcbd1d5e91
Aubree Herrion <aubreeherrion@gmail.com>
2022-10-24 14:33:16 -0500

    finished details page

--- server/routes/familytree.router.js
+++ server/routes/familytree.router.js
@@ -14,13 +14,15 @@ router.get('/', async (req, res) => {
     const result= await pool.query(queryText, [req.user.id]);
     const family = result.rows;
     for( let familyMember of family){
-        queryText= `SELECT * FROM "person"  
-        JOIN "pairing" ON pairing.connection_id = person.id
-        WHERE "connection_type_id" = 3 AND pairing.person_id = $1 AND "person"."user_id"= $2 AND "person"."root"=false; `;
+      queryText = `SELECT "person".* FROM "person"  
+      JOIN "pairing" ON pairing.connection_id = person.id
+      WHERE "connection_type_id" = 3 AND pairing.person_id = $1 AND "person"."user_id"= $2 AND "person"."root"=false; `;
 
-        const children = await pool.query(queryText, [familyMember.id, req.user.id])
-        familyMember.children = children.rows
-    }
+      const children = await pool.query(queryText, [familyMember.id, req.user.id])
+      familyMember.children = children.rows
+  }
+
+
     res.send(family)
 
   } else {
@@ -30,18 +32,22 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id',(req,res) => {
-    if (req.isAuthenticated()) {
-        const personId = req.params.id
-        const queryText = `SELECT * FROM "person" WHERE "id"= $1`;
-        pool.query(queryText,[personId]).then(() => {
-            res.sendStatus(201);
-        }).catch((error) => {
-            res.sendStatus(500)
-        })
-      } else {
-        res.sendStatus(403)
-      }
-})
+  if (req.isAuthenticated()) {
+      const personId = req.params.id
+      const queryText = `SELECT * FROM "person" WHERE "id"= $1`;
+      pool.query(queryText,[personId]).then((results) => {
+        if (results && results.rows.length > 0) {
+          res.send(results.rows[0]);
+        } else {
+          res.sendStatus(404);
+        }
+      }).catch((error) => {
+          res.sendStatus(500)
+      })
+    } else {
+      res.sendStatus(403)
+    }
+});
 
 
 /**
--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import  Box  from '@mui/material/Box';
 
 
 function DetailsPage(){
@@ -11,6 +17,17 @@ function DetailsPage(){
     useEffect(() => {
         fetchDetails()
     },[id]);
+    // formats birthdate
+    const formatBirthdate = (birth) => {
+        const date = new Date(birth);
+        return (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
+      }
+
+      // formats date of death
+      const formatDeathdate = (death) => {
+        const date = new Date(death);
+        return (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
+      }
     
      const fetchDetails = () => {
         axios.get(`/api/tree/${id}`).then((response) => {
@@ -24,8 +41,49 @@ function DetailsPage(){
 
      return(
         <div>
-            <pre>{JSON.stringify({personDetail})}</pre>
-            <h2>First Name :{personDetail.id}</h2>
+           
+            <Card sx={{minWidth: 300}}>
+                <CardContent>
+                    <Typography variant='h4'>
+                        {personDetail.firstname} {personDetail.lastname}
+                    </Typography>
+                    <Typography>
+                        First Name: {personDetail.firstname}
+                    </Typography>
+                    <Typography>
+                        Last Name: {personDetail.lastname}
+                    </Typography>
+                    <Typography>
+                        Birth Last Name: {personDetail.lastname_birth}
+                    </Typography>
+                    <Typography>
+                        Gender: {personDetail.gender}
+                    </Typography>
+                    <Typography>
+                        Date of Birth: {formatBirthdate(personDetail.birth)}
+                    </Typography>
+                    <Typography>
+                    Date of Death: {
+                            personDetail.death && formatBirthdate(
+                                (personDetail.death)
+                            )
+                        }
+                        
+                    </Typography>
+
+                    <Typography>
+                        Birth Place: {personDetail.birthplace}
+                    </Typography>
+                    <CardActions>
+                        <Box display="flex" justifyContent="flex-end" alignItems="flex-end" >
+                            <Button variant="contained">Edit</Button>
+                        </Box>
+                    </CardActions>
+                </CardContent>
+                
+            </Card>
+
+
         </div>
      )
 }
